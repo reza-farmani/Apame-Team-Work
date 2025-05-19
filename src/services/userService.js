896d56170fba07511a16d71079f31abfa1142aed
reza-farmani <farmanir862@gmail.com>
2025-05-19 15:16:50 +0330

    Login & signup

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,3 +23,32 @@ export async function addUser(userData) {
     throw error;
   }
 };
+
+
+export async function loginUser(info) {
+  try {
+    if (!info.emailOrPhone || !info.password) {
+      throw new Error('لطفاً ایمیل/شماره تلفن و رمز عبور را وارد کنید');
+    }
+
+    const getUsers = await fetch(`${API_URL}/users`);
+    if (!getUsers.ok) {
+      throw new Error('خطا در دریافت اطلاعات کاربران');
+    }
+
+    const users = await getUsers.json();
+
+    const user = users.find(u => 
+      (u.email === info.emailOrPhone || u.phone === info.emailOrPhone) && 
+      u.password === info.password
+    );
+
+    if (!user) {
+      throw new Error('ایمیل/شماره تلفن یا رمز عبور اشتباه است');
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,40 +1,46 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import MainNav from "./MainNav";
-import Login from "../pages/Login";
 import { motion } from "framer-motion";
-import Logo from '../assets/logo.png' 
+import Logo from '../assets/logo.png';
 
 function Header() {
-    const [showLogin, setShowLogin] = useState(false);
+    const navigate = useNavigate(); 
 
-    function toLogin () {
-        setShowLogin(!showLogin);
+    function toLogin() {
+        navigate("/signup");
     };
 
     return (
-        <header className=' navbar-shadow'>
-
+        <header className='navbar-shadow'>
             <div className="bg-white backdrop-blur-md w-[75%] h-[10vh] m-auto mt-2 rounded-3xl grid grid-cols-2 items-center justify-center">
-
                 <div className='flex items-center pr-7'>
-                    <motion.img initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1 , x: 0}} viewport={{ once: true }} transition={{ duration: 0.8, delay: 0.2 }}  src={Logo} alt="" className='w-24'/>
+                    <motion.img 
+                        initial={{ opacity: 0, x: 50 }} 
+                        whileInView={{ opacity: 1, x: 0 }} 
+                        viewport={{ once: true }} 
+                        transition={{ duration: 0.8, delay: 0.2 }}  
+                        src={Logo} 
+                        alt="" 
+                        className='w-24'
+                    />
                     <div className="bg-"></div>
                 </div>
 
                 <div className="flex items-center justify-end pl-10">
-                    <motion.button initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1 , x: 0}} viewport={{ once: true }} transition={{ duration: 0.8, delay: 0.2 }} onClick={toLogin} className="btn">ثبت نام / ورود</motion.button>
+                    <motion.button 
+                        initial={{ opacity: 0, x: -50 }} 
+                        whileInView={{ opacity: 1, x: 0 }} 
+                        viewport={{ once: true }} 
+                        transition={{ duration: 0.8, delay: 0.2 }} 
+                        onClick={toLogin} 
+                        className="btn">
+                        ثبت نام / ورود
+                    </motion.button>
                 </div>
             </div>
 
-
-
-
             <MainNav />
-
-
-            {showLogin && <Login />}
-
-
         </header>
     )
 }
